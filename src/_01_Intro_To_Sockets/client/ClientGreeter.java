f75d78ae0d8c1699441ccfede975dc1c646d3736
gabrielsong9674 <gracegabrielson@icloud.com>
2021-01-10 19:59:42 -0600

    ClientServerGreeter

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,22 +8,28 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ipAddress = "localhost";
       //2. Create an integer for the server's port number
-      
+      int portNum = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
     
-    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
-         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
-         //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
-         //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
-         //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
-         //9. Close the client's server object
-
+      	try {
+      		//4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
+			Socket socket = new Socket(ipAddress, portNum);
+			//5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
+	         DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
+	        //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
+	         dataOutput.writeUTF("message");
+	        //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
+	         DataInputStream dataInput = new DataInputStream(socket.getInputStream());
+	        //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
+	         System.out.println(dataInput.readUTF());
+	        //9. Close the client's server object
+	         socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
    }
 }
